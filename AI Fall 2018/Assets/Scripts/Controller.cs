ba4baee9d1aeecebe3109a12b3657147ac01ba33
Parth Contractor <parth.contractor94@gmail.com>
2018-09-03 03:36:40 -0400

    Created simple player Controller

---
+++ AI Fall 2018/Assets/Scripts/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(speed * moveHorizontal * Time.deltaTime, 0.0f, speed * moveVertical * Time.deltaTime);
+    }
+}
