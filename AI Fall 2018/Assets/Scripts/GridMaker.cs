380b4619b2557ab20474212a4b44edf3176f6df5
Shantanu Jamble <sj6045@rit.edu>
2018-11-06 02:13:16 -0500

    Added feature for instantiating units and basic grid creation

--- AI Fall 2018/Assets/Scripts/GridMaker.cs
+++ AI Fall 2018/Assets/Scripts/GridMaker.cs
@@ -4,61 +4,21 @@ using UnityEngine;
 using System.IO;
 
 public class Node{
-    int height;
+   
     Vector3 position;
 
-    public int gCost; // cost to travel to node, based on height
-    public int hCost; // based on grid distance
-    public int fCost; //gCost + hCost
-
-    public int gridX; // the x position in the grid
-    public int gridY; // the y position in the grid
-
-    public bool isWalkable; 
-
-    public Node parent;
-
-    public Node(int h,Vector3 pos, int gridXpos, int gridYpos)
+    
+    public Node(Vector3 pos)
     {
-        height = h;
         position = pos;
-        gridX = gridXpos;
-        gridY = gridYpos;
-        if(h == 0 || h<=7||h>=14)
-        {
-            isWalkable = false;
-        }
-        else
-        {
-            isWalkable = true;
-        }
-
-        gCost = height; // g cost is set to the height for now. It should really be the difference in height between the starting node and this node
     }
-
-    public int Height
-    {
-        get { return height; }
-    }
-
-    public Vector3 Position
-    {
-        get
-        {
-            return position;
-        }
-    }
-
-    public int Fcost
-    {
-        get{ return fCost; }
-    }
+    
 }
 
 public class GridMaker : MonoBehaviour {
     public Terrain terrain;
     public Node[,] grid = new Node[100, 100];
-    public LayerMask unWalkableMask;
+  
 
     public Vector2 gridSizeInWorld;
 
@@ -81,31 +41,9 @@ public class GridMaker : MonoBehaviour {
             for (j=0,z=-50;j<100&&z<=50;j++,z++)
             {
 
-                //new Vector3(x,10, z)
-                h = (int)terrain.SampleHeight(new Vector3(x, 0, z)); 
-                
-                // check for unwalkable layer
-                if (Physics.CheckSphere(new Vector3(x, h, z), radius,unWalkableMask))
-                {
-                    h = 0;
-                }
-                
-                //check for making bridges walkable
-                if (((x<=32&&x>=28)&&(z<=1 && z>=-4))) 
-                {
-                    Debug.Log(i + " " + j);
-                    h = 9;
-                }
-                if (((x <= -8 && x >= -10) && (z <= -7 && z >= -15)))
-                {
-                    Debug.Log(i + " " + j);
-                    h = 9;
-                }
-
-
-                grid[i, j] = new Node(h, new Vector3(x, h, z), i, j);
-                sw.Write(grid[i, j].Height + " ");
-
+               
+                grid[i, j] = new Node( new Vector3(x, Terrain.activeTerrain.SampleHeight(new Vector3(x, 0, z)), z));
+               
                 
                     
                 //Debug.Log("" + grid[i, j
@@ -122,81 +60,5 @@ public class GridMaker : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    //get a node from a world point
-    public Node GetNodeFromWorld(Vector3 worldPos)
-    {
-        int x;
-        int y=0;
-        Debug.Log("actual"+worldPos.x + " " + worldPos.y + " " + worldPos.z);
-        for (x=0;x<100; x++)
-        {
-            for (y=0; y < 100; y++)
-            {
-                Debug.Log("node"+grid[x, y].Position.x + " " + grid[x, y].Position.y + " " + grid[x, y].Position.z);
-                if(grid[x,y].Position.x == (int)worldPos.x && grid[x, y].Position.z == (int)worldPos.z)
-                {
-                    break;
-                }
-            }
-        }
-        //return grid[x,y];
-
-        return grid[0, 0];
-    }
-
-    // Get the neighboring nodes that can be travelled to
-    public List<Node> GetNeighbors(Node node)
-    {
-        List<Node> neighbors = new List<Node>();
-
-        for(int x = -1; x <= 1; x++)
-        {
-            for(int y = -1; y <= 1; y++)
-            {
-                //skip this one
-                if (x == 0 && y == 0)
-                {
-                    
-                }
-                else
-                {
-                    int checkX = node.gridX + x;
-                    int checkY = node.gridY + y;
-
-                    // check if it is within the bounds of the grid
-                    if (checkX >= 0 && checkX < gridSizeInWorld.x && checkY >= 0 && checkY < gridSizeInWorld.y)
-                    {
-                        //add it to the grid of neighbors
-                        neighbors.Add(grid[checkX, checkY]);
-                    }
-                }
-                
-            }
-        }
-
-        return neighbors;
-    }
-
-    public List<Node> path;
-    public void OnDrawGizmos()
-    {
-        //Draw Logic
-        if (grid != null)
-        {
-            foreach(Node n in grid)
-            {
-                if (path != null)
-                {
-                    if(path.Contains(n)){
-                        Gizmos.color = Color.gray;
-                        Gizmos.DrawCube(n.Position, Vector3.one);
-                    }
-                }
-            }
-        }
-    } 
+ 
 }
---
+++ AI Fall 2018/Assets/Scripts/Spawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] prefabs;
+    public Camera cam;
+    
+    int current_prefab = 0;
+
+    public LayerMask mask;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+            try
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                Debug.Log("2");
+                if (Physics.Raycast(ray, out hit, mask))
+                {
+                    Debug.Log("3");
+                    Instantiate(prefabs[current_prefab], hit.point, Quaternion.identity);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.StackTrace);
+            }
+        }
+	}
+
+    public void changePrefab(int index)
+    {
+
+        current_prefab = index;
+        Debug.Log("Index=" + current_prefab);
+    }
+   
+}
