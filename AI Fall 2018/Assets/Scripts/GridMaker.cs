2300e441bf434923fdcd1b4de5940528e6d2f95c
Parth Contractor <parth.contractor94@gmail.com>
2018-11-17 16:50:49 -0500

    Intermediate changes Influence maps

--- AI Fall 2018/Assets/Scripts/GridMaker.cs
+++ AI Fall 2018/Assets/Scripts/GridMaker.cs
@@ -3,62 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class Node{
-   
+public class Node {
     Vector3 position;
 
-    
+    public Color32 influenceColor;
+
+    public int influenceOnNode;
+
     public Node(Vector3 pos)
     {
         position = pos;
+        influenceColor = Color.white;
+        influenceOnNode = 0;
     }
-    
 }
 
 public class GridMaker : MonoBehaviour {
     public Terrain terrain;
     public Node[,] grid = new Node[100, 100];
-  
 
-    public Vector2 gridSizeInWorld;
+    private Vector2 gridSizeInWorld;
 
 	// Use this for initialization
 	void Start () {
         Vector3 start = new Vector3(50, 10, -50);
         Vector3 end = terrain.terrainData.size;
         int i, j, x = 50, z = -50;
-        int radius = 1;
         Debug.LogFormat(""+Terrain.activeTerrain.SampleHeight(new Vector3(-47,0,-16)));
 
         gridSizeInWorld = terrain.terrainData.size;
 
-        //writes a grid of node G costs to a text file
-        StreamWriter sw = new StreamWriter("test.txt");
-
-        int h;
         for (i = 0, x = -50; i < 100 && x <= 50; i++,x++)
         {
             for (j=0,z=-50;j<100&&z<=50;j++,z++)
             {
-
-               
-                grid[i, j] = new Node( new Vector3(x, Terrain.activeTerrain.SampleHeight(new Vector3(x, 0, z)), z));
-               
-                
-                    
-                //Debug.Log("" + grid[i, j
-                
+                grid[i, j] = new Node( new Vector3(x, 50, z));
             }
-            
-            sw.WriteLine();
-            //Debug.Log("\n");
         }
-        
-       sw.Close();
-        
-        
 	}
-	
-	// Update is called once per frame
- 
 }
--- AI Fall 2018/Assets/Scripts/Spawner.cs
+++ AI Fall 2018/Assets/Scripts/Spawner.cs
@@ -7,44 +7,43 @@ public class Spawner : MonoBehaviour {
 
     public GameObject[] prefabs;
     public Camera cam;
-    
-    int current_prefab = 0;
 
     public LayerMask mask;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
+    private int current_prefab = 0;
+
+    public List<KeyValuePair<GameObject, int>> spawnedObjects = new List<KeyValuePair<GameObject, int>>();
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            
             try
             {
                 Ray ray = cam.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
-                Debug.Log("2");
                 if (Physics.Raycast(ray, out hit, mask))
                 {
-                    Debug.Log("3");
-                    Instantiate(prefabs[current_prefab], hit.point, Quaternion.identity);
+                    GameObject spawn = Instantiate(prefabs[current_prefab], hit.point, Quaternion.identity) as GameObject;
+                    spawnedObjects.Add(new KeyValuePair<GameObject, int>(spawn, current_prefab + 1));
                 }
             }
             catch (Exception e)
             {
                 Debug.Log(e.StackTrace);
             }
+
+            Debug.Log("Spawned Objects: " + spawnedObjects.Count);
         }
 	}
 
-    public void changePrefab(int index)
+    public List<KeyValuePair<GameObject, int>> GetListOfSpawnedObjects()
     {
-
-        current_prefab = index;
-        Debug.Log("Index=" + current_prefab);
+        return spawnedObjects;
     }
-   
+
+    public void ChangePrefab(int index)
+    {
+        current_prefab = index;
+    }
 }
